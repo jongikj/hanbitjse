bd51e639e31d463fb667fb0ed8422a6aafdb75e8
hb2010 <hb2010@hb2010-PC>
2016-06-10 08:57:55 +0900

    16.6.10

--- scanner/src/branch/Num1.java
+++ scanner/src/branch/Num1.java
@@ -1,16 +1,13 @@
-/**
- * 
- */
 package branch;
 
 import java.util.Scanner;
 
 /**
- * @date   :2016. 6. 9.
+ * @date :2016. 6. 9.
  * @author :장종익
- * @file   :Num1.java
- * @story  :
-*/
+ * @file :Num1.java
+ * @story :
+ */
 public class Num1 {
 	/**
 	 * 저희 반은 학생 수가 3명입니다.
@@ -24,22 +21,56 @@ public class Num1 {
 	 * */
 	public static void main(String[] args) {
 		String name1 = "", name2 = "", name3 = "";
-		int score1 = 0, score2 = 0, score3 = 0, num1 = 0, num2 = 0, num3 = 0;
+		int score1 = 0, score2 = 0, score3 = 0;
+		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("첫번째 점수는?");
+		System.out.print("1번 학생 이름은?");
+		name1 = scanner.next();
+		System.out.print(name1 + "의 점수는?");
 		score1 = scanner.nextInt();
-		System.out.println("두번째 점수는?");
-		score2 = scanner.nextInt();
-		System.out.println("세번째 점수는?");
-		score3 = scanner.nextInt();
+		if(score1 >= 101 || score1 <= -1){
+			System.out.println("0 ~ 100 중 입력해주세요.");
+			return;
+		}
 		
-		if(score1>score2 && score1>score3){
-			if(score2>score3){
-				
+		System.out.print("2번 학생 이름은?");
+		name2 = scanner.next();
+		System.out.print(name2 + "의 점수는?");
+		score2 = scanner.nextInt();
+		if(score2 >= 101 || score2 <= -1){
+			System.out.println("0 ~ 100 중 입력해주세요.");
+			return;
+		}
+
+		System.out.print("3번 학생 이름은?");
+		name3 = scanner.next();
+		score3 = scanner.nextInt();
+		if(score3 >= 101 || score3 <= -1){
+			System.out.println("0 ~ 100 중 입력해주세요.");
+			return;
+		}
+
+		if (score1 > score2 && score1 > score3) {
+			if(score2 > score3){
+				System.out.println("1등 : " + name1 + ", 평균점수 : " + score1 +"\n2등 : "  + name2 + ", 평균점수 : "  + score2 + "\n3등 : " + name3 + ", 평균점수 : " + score3);
+			} else {
+				System.out.println("1등 : " + name1 + ", 평균점수 : " + score1 +"\n2등 : "  + name3 + ", 평균점수 : "  + score3 + "\n3등 : " + name2 + ", 평균점수 : " + score2);	
 			}
 			
+		} else if (score2 > score1 && score2 > score3) {
+			if(score1 > score3){
+				System.out.println("1등 : " + name2 + ", 평균점수 : " + score2 +"\n2등 : "  + name1 + ", 평균점수 : "  + score1 + "\n3등 : " + name3 + ", 평균점수 : " + score3);
+			} else {
+				System.out.println("1등 : " + name2 + ", 평균점수 : " + score2 +"\n2등 : "  + name3 + ", 평균점수 : "  + score3 + "\n3등 : " + name1 + ", 평균점수 : " + score1);	
+			}
 			
+		} else if (score3 > score1 && score3 > score1) {
+			if(score1 > score2){
+				System.out.println("1등 : " + name3 + ", 평균점수 : " + score3 +"\n2등 : "  + name1 + ", 평균점수 : "  + score1 + "\n3등 : " + name2 + ", 평균점수 : " + score2);
+			} else {
+				System.out.println("1등 : " + name3 + ", 평균점수 : " + score3 +"\n2등 : "  + name2 + ", 평균점수 : "  + score2 + "\n3등 : " + name1 + ", 평균점수 : " + score1);	
+			}
 		}
 	}
 }
